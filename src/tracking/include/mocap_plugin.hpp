78dccdab2745dca7769011ef3669ca9c5546da87
letrend <simon.trendel@tum.de>
2016-09-07 11:02:13 +0200

    making the tracking node a rviz panel plugin

---
+++ src/tracking/include/mocap_plugin.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#ifndef Q_MOC_RUN
+// ros
+#include <ros/ros.h>
+#include <rviz/panel.h>
+#include <pluginlib/class_list_macros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+//std
+#include <stdio.h>
+#include <map>
+// qt
+#include <QPainter>
+#include <QCheckBox>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QTableWidget>
+#include <QComboBox>
+#include <QTimer>
+// messages
+#include "communication/CameraControl.h"
+#include "communication/MarkerPosition.h"
+#include <visualization_msgs/Marker.h>
+
+#include "cameraMarkerModel.hpp"
+
+#endif
+
+using namespace std;
+using namespace cv;
+
+class LightWidget : public QWidget {
+Q_OBJECT
+public:
+    LightWidget(QWidget *parent = 0)
+            : QWidget(parent){
+        this->setFixedWidth(20);
+        this->setFixedHeight(20);
+    }
+
+    void setColor(QColor color) {
+        m_color = color;
+    }
+
+protected:
+    virtual void paintEvent(QPaintEvent *){
+        QPainter painter(this);
+        painter.setRenderHint(QPainter::Antialiasing);
+        painter.setBrush(m_color);
+        painter.drawEllipse(0, 0, width(), height());
+    }
+
+private:
+    QColor m_color;
+};
+
+class MocapPlugin : public rviz::Panel {
+Q_OBJECT
+
+public:
+    MocapPlugin(QWidget *parent = 0);
+
+    ~MocapPlugin();
+
+    /**
+     * Load all configuration data for this panel from the given Config object.
+     * @param config rviz config file
+     */
+    virtual void load(const rviz::Config &config);
+
+    /**
+     * Save all configuration data from this panel to the given
+     * Config object.  It is important here that you call save()
+     * on the parent class so the class id and panel name get saved.
+     * @param config rviz config file
+     */
+    virtual void save(rviz::Config config) const;
+
+    /**
+     * This is the callback function for the marker positions,
+     * depending on the cameraID and the cameras state, these are passed to the responsible Models
+     * @param msg MarkerPosition message, containing seq, timestamp, position in image, cameraID
+     */
+    void pipe2function(const communication::MarkerPosition::ConstPtr& msg);
+
+    void sendCameraControl(const communication::CameraControl::ConstPtr &msg);
+
+
+
+public Q_SLOTS:
+    void initializeCameras();
+
+    void streamCamera(int state);
+
+    void changeID(int index);
+
+    void videoCB(const sensor_msgs::ImageConstPtr& msg);
+
+private:
+    void updateId(const communication::MarkerPosition::ConstPtr &msg);
+
+    ros::NodeHandle *nh;
+    bool initialized = false;
+    pair<uint, uint> currentID;
+    ros::AsyncSpinner *spinner;
+    ros::Publisher camera_control_pub, rviz_marker_pub;
+    ros::Subscriber id_sub, video_sub, marker_position_sub;
+    cv_bridge::CvImageConstPtr cv_ptr;
+    Mat img;
+    bool lockWhileWriting = false;
+    enum{
+        toggleVideoStream = 0
+    }cameraControls;
+
+    Matrix4d ModelMatrix;
+    map<int, CameraMarkerModel> camera;
+    map<int, int> cameraState;
+    vector<vector<float>> markerPositions;
+};
--- src/tracking/src/main.cpp
+++ src/tracking/src/main.cpp
@@ -1,103 +1,14 @@
-#include "ncurses_tracking.hpp"
+#include "markertracker.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "tracking_node");
 
-    NCurses_tracking ncurse;
+    MarkerTracker markerTracker;
+    markerTracker.init();
 
-    char cmd;
-    noecho();
-    do{
-        timeout(100);
-        cmd = mvgetch(4,0);
-        switch (cmd){
-            case '0':
-                ncurse.togglePosePublishing();
-                break;
-            case '1':
-                ncurse.streamVideo();
-                break;
-            case '2':
-                ncurse.reinitialize();
-                break;
-            case 's':
-                ncurse.saveCameraImage();
-                break;
-        }
-        ncurse.showCameraInfo();
-    }while( cmd != '9');
-//
-//    char c;
-//    bool running = true;
-//    while (running){
-//        // handle events
-//        sf::Event event;
-//        while (window.pollEvent(event))
-//        {
-//            switch (event.type)
-//            {
-//                // window closed
-//                case sf::Event::Closed:
-//                    running = false;
-//                    break;
-//                case sf::Event::Resized:
-//                    glViewport(0, 0, event.size.width, event.size.height);
-//                    break;
-//            }
-//        }
-//
-//        switch(currentState)
-//        {
-//            case Render: {
-//                pose_sphere = markerTracker.ModelMatrix.cast<float>();
-//                model.updateViewMatrix(window);
-//                // render the object with the updated modelmatrix
-//                Mat img;
-//                model.render(pose_sphere,img);
-//                window.display();
-//
-//                currentState = NextState(currentState);
-//                break;
-//            }
-//            case Publish: {
-//                static uint next_id = 0;
-//                visualization_msgs::Marker marker;
-//                marker.header.frame_id = "map";
-//                marker.header.stamp = ros::Time::now();
-//                marker.header.seq = next_id++;
-//                marker.ns = "markertracker";
-//                marker.id = next_id;
-//                marker.type = visualization_msgs::Marker::CUBE;
-//                marker.action = visualization_msgs::Marker::ADD;
-//                Vector3f position = pose_sphere.topRightCorner(3,1);
-////                if(ros::Time::now().sec%10==0)
-////                    printf("( %.4f, %.4f, %.4f )\n", position(0), position(1), position(2));
-//                Matrix3f pose = pose_sphere.topLeftCorner(3,3);
-//                Quaternionf q(pose);
-//                marker.pose.position.x = position(0);
-//                marker.pose.position.y = position(1);
-//                marker.pose.position.z = position(2);
-//                marker.pose.orientation.x = q.x();
-//                marker.pose.orientation.y = q.y();
-//                marker.pose.orientation.z = q.z();
-//                marker.pose.orientation.w = q.w();
-//                marker.scale.x = 0.01f;
-//                marker.scale.y = 0.01f;
-//                marker.scale.z = 0.01f;
-//                marker.color.r = 0;
-//                marker.color.g = 1;
-//                marker.color.b = 0;
-//                marker.color.a = 1;
-//                marker.lifetime = ros::Duration(30);
-//                rviz_marker_pub.publish(marker);
-//                currentState = NextState(currentState);
-//            }
-//        }
-//    }
-//
-//    window.close();
+    ros::spin();
 
     return 0;
 }
